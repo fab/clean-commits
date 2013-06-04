e893c98a63ac69f5c9825935d41846391d67c0eb
Fab Mackojc <fab.mackojc@gmail.com>
2013-06-03 22:22:35 -0700

    Rails project boilerplate

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fabsays::Application.config.secret_token = 'bdb2b07d919520a8e8423b87eca55f9cb3ebfe17a192abbc37f01157853e8c0af991c5859c795afac90a0adf83a4279b97b6c057741e9d156586c2330711ea25'
